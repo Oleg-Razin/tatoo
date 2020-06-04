d352c166365171860f4351de84180611d96ced49
Oleg Razin <razyax@gmail.com>
2020-06-04 13:48:32 +0300

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-$('.grid').isotope({
-    layoutMode: 'packery',
-    itemSelector: '.grid-item',
-    masonry: {
-        columnWidth: 455
-    },
-    packery: {
-        gutter: 10
-    }
-});
-(function($) {
-    $(function() {
-        $('select').styler();
-    });
-})(jQuery);
+
+// (function($) {
+//     $(function() {
+//         $('select').styler();
+//     });
+// })(jQuery);
 $(document).ready(function() {
+    $('.grid').isotope({
+        layoutMode: 'packery',
+        itemSelector: '.grid-item',
+        masonry: {
+            columnWidth: 455
+        },
+        packery: {
+            gutter: 10
+        }
+    });
+
+    $('select').styler();
+
     $('.image-link').magnificPopup({
         type:'image',
         gallery:     {
@@ -22,16 +26,18 @@ $(document).ready(function() {
         removalDelay : 300,
         mainClass : 'mfp-fade'
     });
+
+    $('.flowing-scroll').on( 'click', function(){
+        var el = $(this);
+        var dest = el.attr('href');
+        if(dest !== undefined && dest !== '') {
+            $('html').animate({
+                    scrollTop: $(dest).offset().top
+                }, 1000
+            );
+        }
+        return false;
+    });
 });
 
-$('.flowing-scroll').on( 'click', function(){
-    var el = $(this);
-    var dest = el.attr('href');
-    if(dest !== undefined && dest !== '') {
-        $('html').animate({
-                scrollTop: $(dest).offset().top
-            }, 1000
-        );
-    }
-    return false;
-});
+
